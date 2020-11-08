1521b2c8dd04f9274b57379b2552a71a0e9dc28d
stuffundefined <sforman157@gmail.com>
2020-11-08 11:47:37 -0500

    Add updates and random numbers

--- main.js
+++ main.js
@@ -1,1 +1,4 @@
-let updates = ['Cave', 'Cow', 'Villager', 'UI', 'Stone', 'Food', 'Cloud']
+let updates = ['Cave', 'Cow', 'Villager', 'UI', 'Stone', 'Food', 'Cloud', 'Bodily Functions', 'Overworld'];
+let num1 = Math.floor(Math.random() * (updates.length - 2));
+let num2 = Math.floor(Math.random() * (updates.length - num1 - 2)) + num1 + 1;
+let num3 = Math.floor(Math.random() * (updates.length - num1 - num2 - 2)) + num2 + 1;
