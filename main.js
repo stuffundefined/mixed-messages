d74f155c172b23f6ab9d29f557ff04d28b7eecf1
stuffundefined <sforman157@gmail.com>
2020-11-08 12:08:09 -0500

    Updates are now properly random and log message

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 let updates = ['Cave', 'Cow', 'Villager', 'UI', 'Stone', 'Food', 'Cloud', 'Bodily Functions', 'Overworld'];
-let num1 = Math.floor(Math.random() * (updates.length - 2));
-let num2 = Math.floor(Math.random() * (updates.length - num1 - 2)) + num1 + 1;
-let num3 = Math.floor(Math.random() * (updates.length - num1 - num2 - 2)) + num2 + 1;
+let num1, num2, num3;
+do {
+    [num1, num2, num3] = [Math.floor(Math.random() * updates.length), Math.floor(Math.random() * updates.length), Math.floor(Math.random() * updates.length)];
+    console.log(num1, num2, num3);
+} while(num1 === num2 || num1 === num3 || num2 === num3);
+console.log(`Next updates:\n${updates[num1]} update\n${updates[num2]} update\n${updates[num3]} update`);
